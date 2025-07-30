932109c78025f84ebe28ac5aba79545f67564a11
Saif Abdelrazek <saif@saifabdelrazek.com>
2025-07-30 22:25:25 +0300

    Add Sanity Studio integration with document schemas for projects, authors, and playlists

---
+++ sanity/schemaTypes/author.ts
@@ -0,0 +1,32 @@
+import {defineField,defineType} from 'sanity'
+import { UserIcon } from 'lucide-react'
+
+export const author = defineType({
+    name:"author",
+    title:"Author",
+    type:"document",
+    icon: UserIcon,
+    fields: [
+        defineField({
+            name: "id",
+            type: "number",
+            readOnly: true,
+        }),
+        defineField({
+            name: "name",
+            type: "string",
+        }),
+        defineField({
+            name: "username",
+            type: "string",
+        }),
+        defineField({
+            name: "image",
+            type: "url"
+        }),
+        defineField({
+            name: "bio",
+            type: "text",
+        }),
+    ],
+})
